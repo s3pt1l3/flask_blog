5dc935473190e9034aa3b513a0a889b140861e51
Sept1le <powerline.studio@gmail.com>
2021-06-24 21:39:13 +0300

    Added posts blueprint

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask.helpers import url_for
 from config import Config
+from posts.blueprint import posts
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+
+# Bluepprints
+app.register_blueprint(posts, url_prefix='/blog')
