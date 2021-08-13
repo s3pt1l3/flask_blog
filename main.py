f704a5af20e55dae084e5082364e012a0263e641
s3pt1l3 <powerline.studio@gmail.com>
2021-08-13 14:39:51 +0300

    feat: moved posts blueprint, added database models

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from app import app
 import views
+from posts.blueprint import posts
+from models import *
+
+# Bluepprints
+app.register_blueprint(posts, url_prefix='/blog')
 
 if __name__ == "__main__":
+    # Creating tables in the database
+    db.create_all()
+    # Launching the flask application
     app.run()
