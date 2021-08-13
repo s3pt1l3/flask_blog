5b8561d9ff93edb1e6e48c64152befd8cfcc4791
s3pt1l3 <powerline.studio@gmail.com>
2021-08-13 14:43:46 +0300

    feat: added posts blueprints

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,7 +1,16 @@
 from flask import Blueprint, render_template
+from models import *
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
+
 @posts.route('/')
-def home():
-    return render_template('posts.html')
+def posts_page():
+    posts = select_all_posts()
+    return render_template('posts.html', posts=posts)
+
+
+@posts.route('/<slug>')
+def post_page(slug):
+    post = select_post_by_slug(slug)
+    return render_template('post_page.html', post=post)
