29078e334f14e1406c887d643e0adb6b6e2a00cc
s3pt1l3 <powerline.studio@gmail.com>
2021-08-13 14:38:54 +0300

    feat: added config values
    added BASE_DIR, DATABASE_PATH, SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS

--- config.py
+++ config.py
@@ -1,2 +1,15 @@
+from os import path
+
+# Getting app directory
+BASE_DIR = path.dirname(path.abspath(__name__))
+
+
 class Config:
+    """
+    Config class. Contains different config values.
+    """
+
     DEBUG = True
+    DATABASE_PATH = path.join(BASE_DIR, path.join('data', 'database.db'))
+    SQLALCHEMY_DATABASE_URI = f'sqlite:///{DATABASE_PATH}'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
