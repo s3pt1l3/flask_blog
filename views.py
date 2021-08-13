c3bab400c3b3233bda1ebf13ff02ed19dab426c6
s3pt1l3 <powerline.studio@gmail.com>
2021-08-13 14:42:35 +0300

    style: added comments

--- views.py
+++ views.py
@@ -4,4 +4,8 @@ from flask import render_template
 
 @app.route('/')
 def index():
+    """
+    Returns index page, the main page of the Flask Blog
+    """
+
     return render_template('index.html')
