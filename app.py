02b0c3bd89fb7171a56d7ae5bf861e21eda869cf
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-09-24 20:26:34 +0300

    Feat(db): add db migrations

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_msearch import Search
+
 from config import Config
 
 # Initializing Flask app
@@ -10,3 +13,8 @@ app.config.from_object(Config)
 
 # Database
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+# Database search
+search = Search()
+search.init_app(app)
