088f1ec6821e7506c616410a2c9205877270f53f
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-09-24 20:27:15 +0300

    Refactor: changed app to blog

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from app import app
+from app import app as blog
 import views
 from posts.blueprint import posts
 from models import *
 
 # Bluepprints
-app.register_blueprint(posts, url_prefix='/blog')
+blog.register_blueprint(posts, url_prefix='/blog')
 
 if __name__ == "__main__":
     # Creating tables in the database
     db.create_all()
     # Launching the flask application
-    app.run()
+    blog.run()
