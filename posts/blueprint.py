bf03608f2cb1de9030c2871abd2b4c9d190cba87
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-09-24 21:18:26 +0300

    Feat: add searching posts feature

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -1,16 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from models import *
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
-
 @posts.route('/')
 def posts_page():
-    posts = select_all_posts()
-    return render_template('posts.html', posts=posts)
+    posts_ = select_all_posts()
+    return render_template('posts.html', posts=posts_)
 
 
 @posts.route('/<slug>')
 def post_page(slug):
     post = select_post_by_slug(slug)
     return render_template('post_page.html', post=post)
+
+
+@posts.route('/search')
+def search():
+    query = request.args.get('search')
+    
+    posts_ = search_posts(query)
+    return render_template('posts.html', posts=posts_)
