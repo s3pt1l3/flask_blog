ca79823eb8656fe29025117da656859a962c1ffb
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-11-04 23:18:14 +0300

    feat(admin): add admin panel

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_msearch import Search
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 from config import Config
 
@@ -18,3 +20,12 @@ migrate = Migrate(app, db)
 # Database search
 search = Search()
 search.init_app(app)
+
+# Admin panel
+from models import *
+admin = Admin(app)
+
+admin.add_view(ModelView(Post, db.session))
+admin.add_view(ModelView(Tag, db.session))
+admin.add_view(ModelView(User, db.session))
+admin.add_view(ModelView(Role, db.session))
